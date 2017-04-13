9307fe9b51b9f245d352fe9eff43d4547d8fc8e0
Foteini Giannaropoulou <fgiannar@dotsoft.gr>
2017-04-13 12:02:04 +0300

    Add user profile layout and functionality

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -2,10 +2,66 @@
 
 
 angular.module('app')
-  .controller('ProfileCtrl', function ($scope, AuthService) {
-      var user = AuthService.user();
-      user.endUser = AuthService.isEndUser();
-      user.mastori = AuthService.isMastori();
-      $scope.me = user;
+  .controller('ProfileCtrl', ['$scope', 'AuthService', 'AppointmentModel', 'RatingModel', 'UserModel', 'toaster',
+  	function ($scope, AuthService, AppointmentModel, RatingModel, UserModel, toaster) {
 
-  });
+  		// we dont wont the changes in user model to be directly saved to $localStorage
+  		// but only on user save success callback
+      $scope.user = new UserModel(AuthService.user());
+
+      $scope.mobileRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+
+      var appointmentQueryParams = {orderby: 'created_at', order: 'desc', user_id: $scope.user.id};
+
+      $scope.appointments = AppointmentModel.query(appointmentQueryParams);
+
+      $scope.loadMoreAppointments = function() {
+	  		if ($scope.busyAp) {
+	  			return;
+	  		}
+	  		$scope.busyAp = true;
+
+	  		var page = $scope.appointments.current_page + 1;
+	  		var queryParams = angular.extend({page: page}, appointmentQueryParams);
+	    	var oldData = $scope.appointments.data;
+
+	    	AppointmentModel.query(queryParams, function (data) {
+	    		$scope.busyAp = false;
+	    		$scope.appointments = data;
+	    		$scope.appointments.data = oldData.concat(data.data);
+	    	});
+    	}
+
+    	$scope.appoointmentRatingInit = function(appointment) {
+        appointment.rating = new RatingModel();
+    	}
+
+    	$scope.submitRating = function(form, appointment) {
+        form.$setPristine(); // clear form
+        form.$setUntouched();
+
+        appointment.rating.mastori_id = appointment.mastori.id;
+        appointment.rating.appointment_id = appointment.id;
+
+        appointment.rating.$saveForMastori(function(saved){
+
+            toaster.pop('success', "Κομπλέ!", "Σε ευχαριστούμε για την κριτική σου! Θα δημοσιευτεί μόλις την εγκρίνει ο μαστορο-admin! By the way, μόλις κέρδισες " + saved.points_rewarded + " μαστοροπόντους!!");
+        }, function(error){
+            toaster.pop('error', "Ουπς!", "Κάτι δεν πήγε καλά.. Ξαναπροσπάθησε παρακαλώ!");
+        });
+    	}
+
+    	$scope.updateUser = function(form) {
+    		form.$setPristine(); // clear form
+        form.$setUntouched();
+
+    		$scope.user.$save(function(saved) {
+    			// update user in localStorage
+    			AuthService.updateUserData(saved);
+        	toaster.pop('success', "Ετοιμος!", "Τα στοιχεία του λογαριασμού σου ανανεώθηκαν με επιτυχία!");
+	      }, function(error){
+	        toaster.pop('error', "Ουπς!", "Κάτι δεν πήγε καλά.. Ξαναπροσπάθησε παρακαλώ!");
+	      });
+    	}
+
+}]);
