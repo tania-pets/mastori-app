48788f89bc2d1453daf2eaceb88d3c6f0b620e36
Foteini Giannaropoulou <fgiannar@dotsoft.gr>
2017-04-13 12:46:18 +0300

    Update user email unique check

--- app/scripts/directives/user_unique.directive.js
+++ app/scripts/directives/user_unique.directive.js
@@ -1,15 +1,17 @@
-angular.module('app').directive('ngUnique', ['Config', 'UserModel', '$http', function (Config, UserModel, $http) {
-  return {
-    require: 'ngModel',
-    link: function (scope, elem, attrs, ctrl) {
-      elem.on('blur', function (evt) {
-        scope.$apply(function () {
-          UserModel.unique({field:attrs.ngUnique, value:elem.val()}, function(res) {
-             ctrl.$setValidity('unique', res.result);
+angular.module('app').directive('ngUnique', ['Config', 'UserModel', '$http', 'AuthService',
+  function (Config, UserModel, $http, AuthService) {
+    return {
+      require: 'ngModel',
+      link: function (scope, elem, attrs, ctrl) {
+        elem.on('blur', function (evt) {
+          scope.$apply(function () {
+            debugger;
+            UserModel.unique({field:attrs.ngUnique, value:elem.val(), user: AuthService.user() ? AuthService.user().id : null}, function(res) {
+               ctrl.$setValidity('unique', res.result);
+            });
           });
         });
-      });
+      }
     }
-  }
   }
 ]);
